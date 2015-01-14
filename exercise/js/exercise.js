0be78c38a7504a2b91c8f50fe8d52f96bfa907a8
DouG Molidor <dev@dougmolidor.com>
2015-01-14 01:40:59 -0800

    delete post confirm prompt implemented and simple styling

--- exercise/js/exercise.js
+++ exercise/js/exercise.js
@@ -40,7 +40,7 @@ var FeedList = React.createClass({
 		var currentPost = this.props.feedData.first(function(o){ return o.postid === this.state.data.currentActivePostId }.bind(this));
 		var feedPosts = this.props.feedData.map(function (feedPost) {
 			return (
-				<FeedPost postData={feedPost} handleCommentLinkClick={this.showPanel} />
+				<FeedPost postData={feedPost} handleCommentLinkClick={this.showPanel} userId={this.props.userId} />
 			);
 		}.bind(this));
 		return (
@@ -59,8 +59,10 @@ var FeedPost = React.createClass({
 		this.props.handleCommentLinkClick(this.props.postData.postid);
 	},
 	render: function() {
+		var deleteBtn = this.props.postData.uid === this.props.userId ? <PostDeleteBtn postId={this.props.postData.postid} /> : "";
 		return (
 			<div className="feedPost">
+				{deleteBtn}
 				<div className="img-avatar">
 					<img src={this.props.postData.avatar} alt={this.props.postData.name}/>
 				</div>
@@ -79,10 +81,55 @@ var FeedPost = React.createClass({
 	}
 });
 
-var PostPanel = React.createClass({
-	handleCommentComposeSubmit: function(){
-
+var PostDeleteBtn = React.createClass({
+	handleClick: function(e){
+		e.preventDefault();
+		//create event to trigger modal prompt
+		window.dispatchEvent(new CustomEvent('delete_post_click', { 'detail': { "postId" : this.props.postId } }));
 	},
+	render: function() {
+		return (
+			<button className="postDeleteBtn close" postId={this.props.postid} onClick={this.handleClick} type="button" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		)
+	}
+});
+
+var PostDeletePromptModal = React.createClass({
+	handleCancelClick: function(e){
+		e.preventDefault();
+		window.dispatchEvent(new CustomEvent('delete_post_cancel', { 'detail': { "postId" : this.props.postId } }));
+		return false;
+	},
+	handleConfirmClick: function(e){
+		e.preventDefault();
+		window.dispatchEvent(new CustomEvent('delete_post', { 'detail': { "postId" : this.props.postId } }));
+		return false;
+	},
+	render: function() {
+		var modalStyle = this.props.show ? { "display" : "block" } : null;
+		var modalClass = this.props.show ?  "modal fade in" : "modal fade";
+		return (
+			<div className={modalClass} style={modalStyle} onClick={this.handleCancelClick}>
+			  <div className="modal-dialog">
+			    <div className="modal-content">
+			      <div className="modal-header">
+			        <h4 className="modal-title">Delete</h4>
+			      </div>
+			      <div className="modal-body">
+			        <p>Are you sure you want to delete this post?</p>
+			      </div>
+			      <div className="modal-footer">
+			        <button type="button" className="btn btn-default" onClick={this.handleConfirmClick}>Yes</button>
+			        <button type="button" className="btn btn-default" onClick={this.handleCancelClick}>No</button>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+		)
+	}
+});
+
+var PostPanel = React.createClass({
 	render: function() {
 		console.log(this.props);
 		return this.props.currentPost ? (
@@ -102,7 +149,7 @@ var PostPanel = React.createClass({
 				<CommentList comments={this.props.currentPost.comments} />
 				<CommentComposeForm postId={this.props.currentPost.postid} />
 			</div>
-		) : (<div />);
+		) : (null);
 	}
 });
 
@@ -116,7 +163,7 @@ var CommentList = React.createClass({
 		return (
 			<div className="commentList">
 				<p>Comments:</p>
-				{ comments }				
+				{ comments }
 			</div>
 		)
 	}
@@ -147,7 +194,7 @@ var CommentComposeForm = React.createClass({
 	handleChange: function(e) {
 		var submitNode = this.refs.submit.getDOMNode();
 		e.target.value.length > 0 ? submitNode.removeAttribute("disabled") : submitNode.setAttribute("disabled",true);
-  },
+	},
 	handleSubmit: function(e) {
 		e.preventDefault();
 		var commentText = this.refs.commentText.getDOMNode().value.trim();
@@ -193,7 +240,8 @@ var CommentApp = React.createClass({
 	getInitialState: function() {
 		return { "data" : {
 			"feed" : feedData,
-			"user" : userData
+			"user" : userData,
+			"modal" : {}
 			}
 		}
 	},
@@ -204,8 +252,8 @@ var CommentApp = React.createClass({
 			"timestamp" : moment().format('YYYY-MM-DD HH:mm:ss'),
 			"content" : e.detail.commentText
 		};
-		var newState = this.state.data;
-		newState.feed.forEach(function(o){
+		var newState = this.state;
+		newState.data.feed.forEach(function(o){
 			if( o.postid === e.detail.postId ) {
 				o.comments.push(newComment);
 				return;
@@ -213,17 +261,46 @@ var CommentApp = React.createClass({
 		});
 		this.setState(newState);
 	},
+	handleDeletePostClick: function(e) {
+		var newState = this.state;
+		newState.data.modal = { 
+			"showModal" : true,
+			"currentPostId" : e.detail.postId
+		};
+		this.setState(newState);
+	},
+	handleDeletePostCancel: function(e) {
+		var newState = this.state;
+		newState.data.modal = {};
+		this.setState(newState);
+	},
+	handleDeletePost: function(e) {
+		var newState = this.state;
+		newState.data.modal = {};
+		newState.data.feed = newState.data.feed.filter(function(o){
+			return o.postid !== e.detail.postId;
+		});
+		this.setState(newState);
+	},
 	componentDidMount: function() {
-    window.addEventListener('comment_added', this.handleCommentAdded);
-  },
-  componentWillUnmount: function() {
-    window.removeEventListener('comment_added', this.handleCommentAdded);
-  },
+		window.addEventListener('comment_added', this.handleCommentAdded);
+		window.addEventListener('delete_post_click', this.handleDeletePostClick);
+		window.addEventListener('delete_post_cancel', this.handleDeletePostCancel);
+		window.addEventListener('delete_post', this.handleDeletePost);
+	},
+	componentWillUnmount: function() {
+		window.removeEventListener('comment_added', this.handleCommentAdded);
+		window.addEventListener('delete_post_click', this.handleDeletePostClick);
+		window.addEventListener('delete_post_cancel', this.handleDeletePostCancel);
+		window.addEventListener('delete_post', this.handleDeletePost);
+	},
 	render: function() {
+		var appClass = "commentApp " + (this.state.data.modal.showModal ? "show-modal" : "");
 		return (
-			<div class="commentApp">
+			<div className={appClass} >
 				<AppHeader userData={this.state.data.user} />
-				<FeedList feedData={this.state.data.feed} />
+				<FeedList feedData={this.state.data.feed} userId={this.state.data.user.uid} />
+				<PostDeletePromptModal show={this.state.data.modal.showModal} postId={this.state.data.modal.currentPostId} />
 			</div>
 		);
 	}
